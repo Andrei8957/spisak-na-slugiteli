0d0381cc26aa587232543c56522aa017678fc0f5
Andrei8957 <andrejdelevski@example.com>
2025-04-20 21:34:31 +0300

    Add project files.

---
+++ spisak na slugiteli/Employee.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spisak_na_slugiteli
+{
+    public class Employee
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    Console.WriteLine("Greshka!");
+                    return;
+                }
+                else
+                {
+                    this.name = value;
+                }
+
+
+            }
+        }
+        private double zaplata;
+        public double Zaplata
+        {
+            get
+            {
+                return zaplata;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    Console.WriteLine("Greshka!");
+                    return;
+                }
+                this.zaplata = value;
+            }
+        }
+        private string dlygnost;
+        public string Dlagnost
+        {
+            get
+            {
+                return dlygnost;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    Console.WriteLine("Greshka!");
+                    return;
+                }
+                dlygnost = value;
+
+            }
+        }
+        private string otdel;
+
+
+        public string Otdel
+        {
+            get
+            {
+                return otdel;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    Console.WriteLine("Greshka!");
+                    return;
+                }
+                otdel = value;
+            }
+        }
+
+        private string elPosha;
+
+
+        public string ElPosha
+        {
+            get { return elPosha; }
+            set
+            {
+                if (value == null || value == "")
+                {
+                    elPosha = "n/a";
+                }
+                else
+                {
+                    elPosha = value;
+                }
+
+            }
+        }
+
+        private int age;
+
+
+        public int Age
+        {
+            get { return age; }
+            set 
+            { 
+                if (value==null || value==0)
+                {
+                    age =-1;
+                }
+                //dopylnenie :)
+                if (value<16)
+                {
+                    Console.WriteLine("Greshni godini");
+                    return;
+                }
+                else
+                {
+                    age = value;
+                }
+                
+            }
+        }
+        public void PrintMe()
+        {
+            Console.WriteLine($"imeto e {this.Name}");
+            Console.WriteLine($"zaplatata e {this.Zaplata:f2}");
+            //Console.WriteLine($"Otdela e {this.Otdel}");
+            //Console.WriteLine($"Dlygnostta e {this.Dlagnost}");
+
+            Console.WriteLine($"El. posha e {this.ElPosha}");
+            Console.WriteLine($"Godinite sa {this.Age}");
+        }
+    }
+}
---
+++ spisak na slugiteli/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spisak_na_slugiteli
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> persons = new List<Employee>();
+            Console.Write("kolko hora she razglegdame - ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+
+                Employee emp = new Employee();//sazdawa se prazen slugitel//taka wikam employee klasa
+                Console.Write("Wywedi ime - ");
+                emp.Name = Console.ReadLine();
+                Console.Write("Wywedi zaplatata - ");
+                emp.Zaplata = double.Parse(Console.ReadLine());
+                Console.Write("Wywedi dlagnost - ");
+                emp.Dlagnost = Console.ReadLine();
+                Console.Write("Wywedi otdel - ");
+                emp.Otdel = Console.ReadLine();
+                Console.Write("Wywedi El. posha - ");
+                emp.ElPosha = Console.ReadLine();
+                Console.Write("Wywedi godini - ");
+                emp.Age = int.Parse(Console.ReadLine());
+                Console.WriteLine("----------------------");
+                persons.Add(emp);
+
+            }
+            //double zaplataMax = double.MinValue;
+            //int mqstoNaPerson = 0;
+            double MaxAvg = double.MinValue;
+
+            string OtdelNaiVisokaZaplata = "";
+            for (int i = 0; i < persons.Count; i++)
+            {
+                double suma = 0;
+                int count = 1;
+                string segashenOtdel;
+                double avg = 0;
+                for (int j = i + 1; j < persons.Count; j++)
+                {
+                    if (persons[i].Otdel == persons[j].Otdel)
+                    {
+
+                        suma = persons[i].Zaplata + persons[j].Zaplata;
+                        count++;
+                    }
+                }
+
+                avg = suma / count;
+
+
+
+                if (avg > MaxAvg)
+                {
+                    MaxAvg = avg;
+                    OtdelNaiVisokaZaplata = persons[i].Otdel;
+                }
+
+
+
+                //if (persons[i].Zaplata > zaplataMax)
+                //{
+                //    zaplataMax = persons[i].Zaplata;
+                //     mqstoNaPerson = i;
+                //}
+
+            }
+
+            Console.WriteLine($"otdela s nai-golqma zaplata e {OtdelNaiVisokaZaplata}");
+            Console.WriteLine($"slugiteli ot otdel {OtdelNaiVisokaZaplata}");
+            for (int i = 0; i < persons.Count; i++)
+            {
+
+                if (OtdelNaiVisokaZaplata == persons[i].Otdel)
+                {
+                    persons[i].PrintMe();
+                    Console.WriteLine("-------------------");
+                }
+
+            }
+
+            for (int i = 0; i < persons.Count - 1; i++)
+            {
+                for (int j = i + 1; j < persons.Count; j++)
+                {
+                    if (persons[i].Zaplata < persons[j].Zaplata)
+                    {
+                        Employee swap = persons[i];
+                        persons[i] = persons[j];
+                        persons[j] = swap;
+                    }
+                }
+            }
+            Console.WriteLine("Sled sled sortirane slugitelite sa: ");
+            for (int i = 0; i < persons.Count; i++)
+            {
+                Console.WriteLine($"{persons[i].Name}");
+            }
+            //for (int i = 0; i < persons.Count; i++)
+            //{
+            //    persons[i].PrintMe();
+            //    Console.WriteLine("--------------------");
+            //}
+            //Popravih nqkoi nesha po zadachata i dobavih drugi nadqvam se taka da e po-dobre :)
+        }
+    }
+}
